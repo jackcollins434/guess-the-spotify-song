444d7cce9f11c9640675e99aac7ba99eacb32772
Jack Collins <jackcollins434@gmail.com>
2024-03-26 01:19:31 -0400

    feat: add templ and chi router

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/jackcollins434/guess-the-spotify-song/cmd/view"
+)
 
 func main() {
-	fmt.Println(1)
+	r := chi.NewRouter()
+	r.Get("/", templ.Handler(view.Home()).ServeHTTP)
+	http.ListenAndServe(":3001", r)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jackcollins434/guess-the-spotify-song
 
 go 1.21.7
+
+require (
+	github.com/a-h/templ v0.2.648 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+)
